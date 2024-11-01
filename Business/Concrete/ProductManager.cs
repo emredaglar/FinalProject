4e4373fe36c4f80b7ac89e5ee442d46647d01773
emredaglar <emre.daglar1@gmail.com>
2024-11-01 23:50:41 +0300

    #

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,14 +1,19 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ValidationException = FluentValidation.ValidationException;
 
 namespace Business.Concrete
 {
@@ -24,11 +29,17 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
+            //validation
+
+            ValidationTool.Validate(new ProductValidator(), product);
+
+
+
             _productDal.Add(product);
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            //if (product.ProductName.Length<2)
+            //{
+            //    return new ErrorResult(Messages.ProductNameInvalid);
+            //}
 
             //add i geçmiş mutlu. şimdi result çalıştırabilir
             return new SuccessResult(Messages.ProductAdded);
@@ -40,28 +51,28 @@ namespace Business.Concrete
             //iş kodlarından geçtiğini varsayıyoruz.
             //ifler vs. yetkisi var mı? varsa.-->
 
-            if (DateTime.Now.Hour==11)
+            if (DateTime.Now.Hour == 11)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
             //return _productDal.GetAll();
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
 
         }
         //----------------------sonrası----------------//
         public IDataResult<List<Product>> getAllByCategoryId(int id)
         {//( )  bana bir lambda yani kural ver diyor.
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId == id));
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
         }
 
         public IDataResult<Product> getById(int id)
         {
-            return new SuccessDataResult<Product>(_productDal.Get(p=>p.ProductId == id));
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == id));
         }
 
         public IDataResult<List<Product>> getByUnitPrice(decimal min, decimal max)
         {
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.UnitPrice >= min && p.UnitPrice <= max));
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
